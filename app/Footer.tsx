6ce87d8dfd62b9d88160e40912a3620c05753d9a
Kelly Nkohla <kellynkohla0@gmail.com>
2025-12-11 23:17:26 +0200

    styling leagues

--- app/Footer.tsx
+++ app/Footer.tsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <main>
+        <div>
+            <footer className="bg-black text-white text-center p-4 mt-4">
+                &copy; {new Date().getFullYear()} Touchies. All rights reserved.
+            </footer>
+        </div>
+    
+    
+    </main>
+    
   )
 }
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import Link from 'next/link'
+import Link from 'next/link';
 
 export default function Home() {
   return (
@@ -7,7 +7,51 @@ export default function Home() {
       <div>
         <div >        
           <h1 className="align-items:center justify-center flex text-2xl p-3 m-3">Welcome to Touchies!</h1>
-          <h2 className="text-lg">Latest News</h2>
+        </div>
+        <div>
+          <a href="#" className="flex flex-col items-center bg-neutral-primary-soft p-6 m-5 border border-default rounded-base shadow-xs md:flex-row md:max-w-xl md:flex-row md:max-w-xl">
+              <img className="object-cover w-full rounded-base h-64 md:h-auto md:w-48 mb-4 md:mb-0" src="/docs/images/blog/image-4.jpg" alt="" />
+              <div className="flex flex-col justify-between md:p-4 leading-normal">
+                  <h5 className="mb-2 text-2xl font-bold tracking-tight text-heading">Streamlining your design process today.</h5>
+                  <span className="inline-flex items-center bg-brand-softer border border-brand-subtle text-fg-brand-strong text-xs font-medium px-1.5 py-0.5 rounded-sm">
+                    <svg className="w-3 h-3 me-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M18.122 17.645a7.185 7.185 0 0 1-2.656 2.495 7.06 7.06 0 0 1-3.52.853 6.617 6.617 0 0 1-3.306-.718 6.73 6.73 0 0 1-2.54-2.266c-2.672-4.57.287-8.846.887-9.668A4.448 4.448 0 0 0 8.07 6.31 4.49 4.49 0 0 0 7.997 4c1.284.965 6.43 3.258 5.525 10.631 1.496-1.136 2.7-3.046 2.846-6.216 1.43 1.061 3.985 5.462 1.754 9.23Z"/></svg>
+                    Trending/Latest News
+                  </span>
+                  <p className="mb-6 text-body">In todays fast-paced digital landscape, fostering seamless collaboration among Developers and IT Operations.</p>
+                  <div>
+                      <button type="button" className="inline-flex items-center w-auto text-body bg-neutral-secondary-medium box-border border border-default-medium hover:bg-neutral-tertiary-medium hover:text-heading focus:ring-4 focus:ring-neutral-tertiary shadow-xs font-medium leading-5 rounded-base text-sm px-4 py-2.5 focus:outline-none">
+                          Read more
+                          <svg className="w-4 h-4 ms-1.5 rtl:rotate-180 -me-0.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 12H5m14 0-4 4m4-4-4-4"/></svg>
+                      </button>
+                  </div>
+              </div>
+          </a>
+        </div>
+        <div className="bg-neutral-primary-soft block max-w-sm border border-default rounded m-5 shadow-xs">
+            <a href="/news">
+                <img className="rounded-t-base" src="/docs/images/blog/image-1.jpg" alt="" />
+            </a>
+            <div className="p-6 text-center">
+                <a href="#">
+                    <h5 className="mt-3 mb-6 text-2xl font-semibold tracking-tight text-heading">{/*title*/}Touch Rugby at Zoolake</h5>
+                </a>
+                <Link href="/news" className="inline-flex items-center text-black bg-brand box-border border border-transparent hover:bg-brand-strong focus:ring-4 focus:ring-brand-medium shadow-xs font-medium leading-5 rounded-base text-sm px-4 py-2.5 focus:outline-none">
+                    Explore more 
+                    <svg className="w-4 h-4 ms-1.5 rtl:rotate-180 -me-0.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 12H5m14 0-4 4m4-4-4-4"/></svg>
+                </Link>
+            </div>
+        </div>
+        <div className="bg-neutral-primary-soft block max-w-sm p-6 border border-default rounded shadow-xs m-5">
+            <h5 className="mb-3 text-2xl font-semibold tracking-tight text-heading leading-8">Leagues</h5>
+            <p className="text-body mb-6">Touch Leagues in South Africa</p>
+            <ul>
+              <li>6 Down</li>
+              <li>One Touch</li>
+              <li>Beach Touch</li>
+            </ul>
+            <Link href="leagues" className="inline-flex items-center text-black bg-brand box-border border border-transparent hover:bg-brand-strong focus:ring-4 focus:ring-brand-medium shadow-xs font-medium leading-5 rounded-base text-sm px-4 py-2.5 focus:outline-none">
+                Explore more
+            </Link>
         </div>
       </div>
     </main>
